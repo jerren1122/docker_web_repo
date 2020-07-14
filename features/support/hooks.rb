932a90c57ca36b167d5365480fd2bce58d0899a9
Jerren Every <jerren1122@hotmail.com>
2020-07-14 13:45:44 -0400

    Adding testing file

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,6 +1,9 @@
 Before do |scenario |
-  options = Selenium::WebDriver::Chrome::Options.new
-  options.headless!
+  Selenium::WebDriver::Chrome.path = browser_executable_location
+  chrome_options = Selenium::WebDriver::Chrome::Options.new
+  chrome_options.add_argument('--no-sandbox')
+  chrome_options.add_argument('--disable-dev-shm-usage')
+  chrome_options.headless!
   @browser = Watir::Browser.new(:chrome, {options: options})
   @browser.goto('https://www3.mtb.com/')
 
--- lib/helpers/page_helper.rb
+++ lib/helpers/page_helper.rb
@@ -5,6 +5,10 @@ module PageHelper
     Kernel.const_get(page_name.split(' ').collect { |a| (a =~ /\A\z/i ? a.upcase : a.capitalize) }.join('') + 'Page')
   end
 
+  def browser_executable_location
+    ENV['browser_executable_location'].nil? ? false : ENV['browser_executable_location']
+  end
+
   def to_element(element)
       @current_page.instance_eval("#{element.gsub(' ', '_').gsub('/', '.').downcase}_element")
   end
