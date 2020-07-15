6c408b8ac81001fb05429bcbca167281079495f0
Jerren Every <jerren1122@hotmail.com>
2020-07-15 16:49:04 -0400

    Added compose file and yaml

---
+++ docker_run_script.cmd
@@ -0,0 +1,5 @@
+docker-compose up
+
+docker container cp sample_cucumber_two_1:web_repo/output ./docker_output/2
+docker container cp sample_cucumber_one_1:web_repo/output ./docker_output/1
+docker container cp sample_cucumber_three_1:web_repo/output ./docker_output/3
