ffe778218e69d61065327604116a1d32250a0c64
Jerren Every <jerren1122@hotmail.com>
2020-07-19 21:26:02 -0400

    Going up to 10 executors in parallel

--- docker_run_script.cmd
+++ docker_run_script.cmd
@@ -3,9 +3,8 @@ set starting_date=%date%
 
 docker-compose rm -f
 docker-compose up
-set file_name = repo_name
 
-docker container cp %file_name%_one_1:web_repo/output ./docker_output/1
+docker container cp sample_cucumber_one_1:web_repo/output ./docker_output/1
 docker container cp sample_cucumber_two_1:web_repo/output ./docker_output/2
 docker container cp sample_cucumber_three_1:web_repo/output ./docker_output/3
 docker container cp sample_cucumber_four_1:web_repo/output ./docker_output/4
