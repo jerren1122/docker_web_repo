89dd0c939dd3f46790e7c64ce027f4834ba9a3be
Jerren Every <jerren1122@hotmail.com>
2020-08-03 12:22:06 -0400

    Change back to old school dockerfile and docker-compose

--- docker_compose_run_script.cmd
+++ docker_compose_run_script.cmd
@@ -4,8 +4,6 @@ set starting_date=%date%
 docker-compose rm -f
 docker-compose up
 
-
-
 docker container cp sample_cucumber_one_1:web_repo/output ./docker_output/1
 docker container cp sample_cucumber_two_1:web_repo/output ./docker_output/2
 docker container cp sample_cucumber_three_1:web_repo/output ./docker_output/3
